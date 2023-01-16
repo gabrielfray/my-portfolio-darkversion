faf7de51e67c47250a4ed2e15ff566038a99ebd3
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2023-01-16 14:37:01 -0300

    feat: Start and finish project portfolio

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Gabriel Fray",
+  githubUser: "GabrielFray",
+  linkedinUser: "gabrielfray",
+  whatsappNumber: "+5519997072653",
+  emailUser: "gabrielfraygarandy@gmail.com",
+  instagramUser: "gabrielfray_dev",
   facebookUser: "",
 };
