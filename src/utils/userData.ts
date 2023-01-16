bd255f7d8e126eff5f256821658352158d4f44bf
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2023-01-16 14:23:34 -0300

    Initial commit

---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: "Samuel Leão",
+  githubUser: "samuelleaokenzie",
+  linkedinUser: "nekelpatrick",
+  whatsappNumber: "+5541988819028",
+  emailUser: "samuel@kenzie.com.br",
+  instagramUser: "samuelleaoui",
+  facebookUser: "",
+};
